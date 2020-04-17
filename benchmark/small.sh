c4c9196ef1179046da315b8c5d1f9316b29b1cad
alexiszamanidis <sdi1600047@di.uoa.gr>
2020-04-18 00:30:11 +0300

    add benchmark scripts

---
+++ benchmark/small.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+function print_green {
+    reset='\033[0m'
+    green='\033[0;32m'
+    echo -e "${green}$1${reset}"
+}
+
+clear
+
+# compile
+cd ../source && make 2>&1 > /dev/null
+
+# run benchmarks
+print_green "Threads in thread pool: 1"
+echo $(./join 1 < ../inputfiles/inputfile_small | tail -1)
+print_green "Threads in thread pool: 2"
+echo $(./join 2 < ../inputfiles/inputfile_small | tail -1)
+print_green "Threads in thread pool: 3"
+echo $(./join 3 < ../inputfiles/inputfile_small | tail -1)
+print_green "Threads in thread pool: 4"
+echo $(./join 4 < ../inputfiles/inputfile_small | tail -1)
+
+# clean
+make clean 2>&1 > /dev/null
